1dc9ab7dd9a4923ec86101c58ff39f732d831a63
NaHa01 <54594088+NaHa01@users.noreply.github.com>
2025-01-13 19:07:17 +0900

    [Bronze V] Title: 세제곱의 합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.py
@@ -0,0 +1,10 @@
+N = int(input())
+result1 = 0
+result3 = 0
+for i in range(1,N+1) :
+    result1 += i
+for i in range(1,N+1) :
+    result3 += (i**3)
+print(result1)
+print(result1**2)
+print(result3)
